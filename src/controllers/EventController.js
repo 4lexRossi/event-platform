d88daf10971ef52629f95b51f0dbe0c54c5180f3
4lexRossi <devalexrossi@gmail.com>
2020-07-06 23:22:12 -0300

    criando a função de login e validação dos campos com MongoDB

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 module.exports = {
   async createEvent(req, res){
-    const { title, description, price } = req.body;
+    const { title, description, price, sport } = req.body;
     const { user_id }= req.headers;
     const { filename } = req.file;
 
@@ -15,22 +15,23 @@ module.exports = {
     const event = await Event.create({
       title,
       description,
+      sport,
       price: parseFloat(price),
       user: user_id,
       thumbnail: filename 
     })
     return res.json(event);
-  },
+  },  
   
-  async getEventById(req, res){
+  async delete(req, res) {
     const { eventId } = req.params;
-      try {
-        const event = await Event.findById(eventId)
-          if (event){
-            return res.json(event)
-          }       
-      } catch (error) {
-        return res.status(400).json({ message: 'Evento não existe!' })       
-      }   
+    try {
+      await Event.findByIdAndDelete(eventId)
+      return res.status(204).send()
+
+    } catch (error) {
+      return res.status(400).json({ message: 'Não existem um evento com esse ID'})  
+    }
+    
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 
 const UserController = require('./controllers/UserController');
 const EventController = require('./controllers/EventController');
+const DashboardController = require('./controllers/DashboardController');
+const LoginController = require('./controllers/LoginController');
 const uploadConfig = require('./config/upload');
 
 const routes = express.Router();
@@ -11,11 +13,21 @@ const upload = multer(uploadConfig);
 routes.get('/status', (req, res) => {
   res.send({ status: 200 });
 })
+  //login
+routes.post('/login', LoginController.store)
+  
+  //Dashboards
+routes.get('/dashboard/:sport', DashboardController.getAllEvents)
+routes.get('/dashboard', DashboardController.getAllEvents)
+routes.get('/event/:eventId', DashboardController.getEventById)
 
+  //Events
 routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
-routes.get('/event/:eventId', EventController.getEventById)
+routes.delete('/event/:eventId', EventController.delete)
 
+  //User
 routes.post('/user/register', UserController.createUser)
 routes.get('/user/:userId', UserController.getUserById)
+
 
 module.exports = routes;
